1ee2a2478c09526a1bc14612305b2d19ed5cc195
Wahid <wahid@girtu.com>
2019-02-19 17:26:27 +0800

    build

---
+++ build/precache-manifest.e954ff1dc505988f0199bbb0dd127961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/material-sense/static/media/shape.208f3d38.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "09f4e86c218a577ad5da",
+    "url": "/material-sense/static/js/runtime~main.09f4e86c.js"
+  },
+  {
+    "revision": "012b88674677b02abac3",
+    "url": "/material-sense/static/js/main.012b8867.chunk.js"
+  },
+  {
+    "revision": "6eb75c7a2dfd970c3019",
+    "url": "/material-sense/static/js/1.6eb75c7a.chunk.js"
+  },
+  {
+    "revision": "012b88674677b02abac3",
+    "url": "/material-sense/static/css/main.1b2415d2.chunk.css"
+  },
+  {
+    "revision": "b832bf01340ae101f957342dc2a7c6f0",
+    "url": "/material-sense/index.html"
+  }
+];
