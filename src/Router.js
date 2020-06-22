854f0f4db97b445dbf7a0bc5348cfc94bc577ffc
wahid <wahid@girtu.com>
2020-06-22 17:32:45 +0800

    react-router-dom

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+export default () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/detail" component={Detail}/>
+    </Switch>
+  </HashRouter>
+);
---
+++ src/pages/Detail.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Detail extends Component {
+  render() {
+    return (
+      <div>
+        <a href='#/'>回到Home</a>
+      </div>
+    )
+  }
+}
