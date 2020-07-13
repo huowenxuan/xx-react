c6a2eaf635328afc137c67bad94938f932ce24df
wahid <wahid@girtu.com>
2020-07-13 15:50:36 +0800

    pure component

--- src/components/EditBottom/EditBottomButtons/index.js
+++ src/components/EditBottom/EditBottomButtons/index.js
@@ -1,36 +1,28 @@
-import React, {PureComponent} from "react"
+import React, {PureComponent, useEffect} from "react"
+import {withStateHandlers, compose, pure} from "recompose"
+
 import './index.css'
 import images from "../../../assets/images"
 
 export const EditBottomHeight = 63
-export default class EditBottom extends PureComponent {
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this._show()
-    // }, 1000)
-  }
-
-  show = () => {
+const EditBottom = (props) => {
+  const show = () => {
     // overlays.show(<EditBottomOverlay/>)
   }
 
-  onLeftClick = (e) => {
-    const {onLeftClick, onRightClick} = this.props
+  const onLeftClick = (e) => {
+    const {onLeftClick, onRightClick} = props
     e.stopPropagation()
     onLeftClick && onLeftClick()
   }
 
-  onRightClick = (e) => {
-    const {onLeftClick, onRightClick} = this.props
+  const onRightClick = (e) => {
+    const {onLeftClick, onRightClick} = props
     e.stopPropagation()
     onRightClick && onRightClick()
   }
 
-  _renderBtn = (text, isActive, activeImg, unActiveImg, action, desc, style) => {
+  const _renderBtn = (text, isActive, activeImg, unActiveImg, action, desc, style) => {
     // 设置权限永远居中，音乐：没音乐则居中，有音乐就居左，设置marinleft
     return (
       <div className='edit-btn-box'>
@@ -44,32 +36,29 @@ export default class EditBottom extends PureComponent {
           <p className='edit-btn-desc'>{desc}</p>
         </button>
       </div>
-
     )
   }
 
-  render() {
-    const {active} = this.props
-    return (
-      <div id='edit-bottom' style={{height: EditBottomHeight}}>
-        {this._renderBtn(
-          '背景音频',
-          active === 'music',
-          images.icon_music_red,
-          images.icon_music,
-          this.onLeftClick,
-          '残影月 - faded钢琴独奏版',
-          {marginLeft: 50}
-        )}
-        {this._renderBtn(
-          '谁可以看',
-          active === 'permission',
-          images.icon_limit_red,
-          images.icon_limit,
-          this.onRightClick,
-          '公开'
-        )}
-      </div>
-    )
-  }
+  return (
+    <div id='edit-bottom' style={{height: EditBottomHeight}}>
+      {_renderBtn(
+        '背景音频',
+        props.active === 'music',
+        images.icon_music_red,
+        images.icon_music,
+        onLeftClick,
+        '残影月 - faded钢琴独奏版',
+        {marginLeft: 50}
+      )}
+      {_renderBtn(
+        '谁可以看',
+        props.active === 'permission',
+        images.icon_limit_red,
+        images.icon_limit,
+        onRightClick,
+        '公开'
+      )}
+    </div>
+  )
 }
+export default compose(pure)(EditBottom)
