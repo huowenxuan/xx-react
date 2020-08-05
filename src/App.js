9c0e3ed47f6ddc763bb8529f586cce62b580caa7
霍文轩 <1078954008@qq.com>
2020-08-05 16:26:04 +0800

    修复topview在下

--- src/App.js
+++ src/App.js
@@ -5,51 +5,53 @@ import TopView from "./components/overlays/TopView"
 import Fixed from "./components/Fixed"
 import ReactDOM from "react-dom"
 import 'lib-flexible'
-import {synchronize} from './utils'
-
-let node = document.createElement("div")
-document.body.appendChild(node)
-ReactDOM.render(
-  <Fixed><TopView/></Fixed>,
-  node
-)
 
 export default ({globalEventDistributor, history, store }) => {
-  const [globalState, setGlobalState] = useState(globalEventDistributor.getState())
-  // const [globalState, setGlobalState] = useState({
-  //   loginReducer: {
-  //     userId: '1'
-  //   }
-  // })
+  useState(()=>{
+    // 初始化后再加入，防止顺序在上，无法把app覆盖住
+    let node = document.createElement("div")
+    document.body.appendChild(node)
+    ReactDOM.render(
+      <Fixed><TopView/></Fixed>,
+      node
+    )
+  }, [])
 
-  useEffect(()=>{{
-    setGlobalState(globalEventDistributor.getState())
-  }}, [globalEventDistributor])
+  const [globalState, setGlobalState] = useState({
+    loginReducer: {
+      userId: '1'
+    }
+  })
 
-  console.log("***********");
-  console.log(globalEventDistributor.getState());
-  console.log(globalEventDistributor);
-  if (!globalEventDistributor.getState().loginReducer.userId) {
-    synchronize().then((s) => {
-      console.log(":::::::::::::::::::");
-      console.log(s);
-      console.log(globalEventDistributor.getState());
-      console.log(":::::::::::::::::::");
-      if (globalEventDistributor) {
-        globalEventDistributor.dispatch({
-          type: "login",
-          payload: {
-            userId: s.userId,
-            token: s.token,
-            userName: s.userName,
-            phone: s.phone,
-            openid: s.openid,
-            avatar: s.avatar,
-          },
-        });
-      }
-    });
-  }
+  // const [globalState, setGlobalState] = useState(globalEventDistributor.getState())
+  // useEffect(()=>{{
+  //   setGlobalState(globalEventDistributor.getState())
+  // }}, [globalEventDistributor])
+  //
+  // console.log("***********");
+  // console.log(globalEventDistributor.getState());
+  // console.log(globalEventDistributor);
+  // if (!globalEventDistributor.getState().loginReducer.userId) {
+  //   synchronize().then((s) => {
+  //     console.log(":::::::::::::::::::");
+  //     console.log(s);
+  //     console.log(globalEventDistributor.getState());
+  //     console.log(":::::::::::::::::::");
+  //     if (globalEventDistributor) {
+  //       globalEventDistributor.dispatch({
+  //         type: "login",
+  //         payload: {
+  //           userId: s.userId,
+  //           token: s.token,
+  //           userName: s.userName,
+  //           phone: s.phone,
+  //           openid: s.openid,
+  //           avatar: s.avatar,
+  //         },
+  //       });
+  //     }
+  //   });
+  // }
 
   return (
     <div className='App'>
