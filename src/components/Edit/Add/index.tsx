9b99d7fd1773c590d5a4a870c02c9514f5054766
霍文轩 <1078954008@qq.com>
2020-09-03 14:44:38 +0800

    添加的弹出框如果超出屏幕则显示到上方

--- src/components/Edit/Add/index.tsx
+++ src/components/Edit/Add/index.tsx
@@ -1,15 +1,16 @@
-import OverlayViewFade from "../../overlays/OverlayViewFade";
-import React from "react";
+import OverlayViewFade from "../../overlays/OverlayViewFade"
+import React from "react"
 import './index.less'
-import images from "../../../assets/images";
+import images from "../../../assets/images"
+
 export default (props) => {
   const width = 267
   const {rect, onDismiss, onImage, onText, onLink, onVideo} = props
   const actions = [
     {text: '照片', action: () => _action(onImage), icon: images.add_photo_icon},
-    {text: '文字', action: () => _action(onText),  icon: images.add_text_icon},
-    {text: '链接', action: () => _action(onLink),  icon: images.add_link_icon},
-    {text: '视频', action: () => _action(onVideo),  icon: images.add_video_icon},
+    {text: '文字', action: () => _action(onText), icon: images.add_text_icon},
+    {text: '链接', action: () => _action(onLink), icon: images.add_link_icon},
+    {text: '视频', action: () => _action(onVideo), icon: images.add_video_icon},
   ]
 
   const _action = (action) => {
@@ -18,16 +19,58 @@ export default (props) => {
   }
 
   const _renderActions = () => {
-    return actions.map(({text, action, icon}) => (
+    return <div
+      onTouchStart={e => e.stopPropagation()}
+      className='actions'
+    >
+      {actions.map(({text, action, icon}) => (
+        <div
+          onClick={action}
+          key={text}
+          className='action'
+        >
+          <img className='icon' src={icon}/>
+          {text}
+        </div>
+      ))}
+    </div>
+  }
+
+
+  const renderBottomPop = () => {
+    return (
       <div
-        onClick={action}
-        key={text}
-        className='action'
+        className='box'
+        style={{
+          width,
+          top: rect.top + rect.height,
+          left: rect.left + rect.width / 2 - width / 2
+        }}
       >
-        <img  className='icon' src={icon}/>
-        {text}
+        <div className='triangle-box bottom'>
+          <div className='triangle bottom'/>
+        </div>
+        {_renderActions()}
       </div>
-    ))
+    )
+  }
+
+  const renderTopPop = () => {
+    return (
+      <div
+        className='box'
+        style={{
+          width,
+          top: rect.top - rect.height - 26,
+          left: rect.left + rect.width / 2 - width / 2
+        }}
+      >
+        {_renderActions()}
+        <div className='triangle-box top'>
+          <div className='triangle top'/>
+        </div>
+      </div>
+    )
   }
 
   return (
@@ -41,24 +84,10 @@ export default (props) => {
         onTouchStart={onDismiss}
         className='edit-add-overlay'
       >
-        <div
-          className='box'
-          style={{
-            width,
-            top: rect.top + rect.height,
-            left: rect.left + rect.width / 2 - width / 2
-          }}
-        >
-          <div className='triangle-box'>
-            <div className='triangle'/>
-          </div>
-          <div
-            onTouchStart={e => e.stopPropagation()}
-            className='actions'
-          >
-            {_renderActions()}
-          </div>
-        </div>
+        {(rect.y + rect.height) >= (window.innerHeight - rect.height)
+          ? renderTopPop()
+          : renderBottomPop()
+        }
 
       </div>
     </OverlayViewFade>
